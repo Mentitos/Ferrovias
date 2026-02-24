4c01f45401377f8785bf3edc07b99b51011a9b8a
Mentitos <yomatiastello@gmail.com>
2026-02-24 01:57:33 -0300

    Final commit de momento

--- check_lengths.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const match = html.match(/const LV_VR = \[([\s\S]*?)\];/);
-const arrayStr = '[' + match[1] + ']';
-const arr = eval(arrayStr);
-const lengths = arr.map(x => x.length);
-const un = [...new Set(lengths)];
-print = console.log;
-print("Lengths in LV_VR:", un);
